1ad8db503ecb272b950770570da75e32ed867d8f
GuiBret <guillaume.bretzner@gmail.com>
2018-01-11 03:36:09 +0100

    ajout service api

---
+++ src/app/api-request.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { ApiRequestService } from './api-request.service';
+
+describe('ApiRequestService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ApiRequestService]
+    });
+  });
+
+  it('should ...', inject([ApiRequestService], (service: ApiRequestService) => {
+    expect(service).toBeTruthy();
+  }));
+});
