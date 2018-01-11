5ea66ecf3234229cda65f87b4da5000a744fe032
GuiBret <guillaume.bretzner@gmail.com>
2018-01-11 18:49:20 +0100

    modif index

---
+++ src/app/api-request/api-request.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    HttpClient
+  ],
+  declarations: []
+})
+export class ApiRequestModule {
+
+    
+}
