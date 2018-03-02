6babd95250f363843af4cc611fc4f8ae40819b0f
GuiBret <guillaume.bretzner@gmail.com>
2018-03-02 20:46:24 +0100

    chore(): Defining titles

--- src/app/competition-info/competition-info.component.ts
+++ src/app/competition-info/competition-info.component.ts
@@ -12,24 +12,26 @@ import 'rxjs/add/operator/switchMap';
 })
 export class CompetitionInfoComponent implements OnInit {
 
-  constructor(private http : HttpClient, private route : ActivatedRoute) { }
+  constructor(private http : HttpClient, private route : ActivatedRoute, private titleService : Title) {
+   }
 
     competition: Object
   ngOnInit() {
-      
+
       this.route.params.subscribe((params : ParamMap) => {
 
             this.http.get(`http://${window.location.hostname}/back-resultats/competition/${params["competitionid"]}`).subscribe((data) => {
+                this.titleService.setTitle(data.caption);
                 sessionStorage.setItem("totalMatchdays", data["numberOfMatchdays"]);
                 sessionStorage.setItem("currentMatchday", data["currentMatchday"]);
                 this.competition = data;
             });
-          
+
       });
-      
-      
-      
-      
+
+
+
+
   }
 
 }
